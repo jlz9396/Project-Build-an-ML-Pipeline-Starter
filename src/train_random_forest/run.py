4792ed0392ad050daaecb16d9a5b0c29f8170f02
jlz9396 <jlz9396@gmail.com>
2024-11-26 19:06:12 -0600

    Update runpy

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -112,7 +112,7 @@ def go(args):
     # Get the columns that we are really using from the pipeline
     # NOTE: I don't use it because we have int columns with nans
     # and these are converted to float, which is a conflict with the signature/schema
-    #signature = infer_signature(X_val[processed_features], y_pred)
+    signature = infer_signature(X_val[processed_features], y_pred)
 
     with tempfile.TemporaryDirectory() as temp_dir:
 
